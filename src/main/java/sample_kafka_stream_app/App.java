1cfac64ca24bb5588153a72d373dc6ae7f2db96a
jacace <jacace@jacaces-MacBook-Air.local>
2021-01-26 13:51:34 +0000

    Removed deser examples

---
+++ src/main/java/sample_kafka_stream_app/App.java
@@ -0,0 +1,13 @@
+package sample_kafka_stream_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sample_kafka_stream_app/AppTest.java
@@ -0,0 +1,20 @@
+package sample_kafka_stream_app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
