098c0221db9f6480ced3675d90ecf16862577932
jacace <javierandrescaceres@gmail.com>
2021-02-03 20:00:25 +0000

    Refactored sample stream

--- src/main/java/sample_kafka_stream_app/App.java
+++ src/main/java/sample_kafka_stream_app/App.java
@@ -2,44 +2,48 @@ package sample_kafka_stream_app;
 
 import java.util.Arrays;
 import java.util.Properties;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+//import org.springframework.core.env.Environment;
 
 /**
- * Hello world!
- *
+ * Tets of confluent cloud kafka!
  */
 public class App {
 
-    private String reqTopicName = "word-count-req";
-    private String resTopicName = "word-count-res";
+    private String resTopicName = "out_topic";
+    //@Autowired
+    //private Environment env;    
+
 
     public static void main(String[] args) {
         new App().SetupAndRunPipeline();
     }
 
     private void SetupAndRunPipeline() {
-        // With High Level DSL
+        //General configuration
         Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "sample_kafka_stream_app");
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "");
-        // config.put(StreamsConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, Config.ConsumerId);        
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, Config.BootstrapServers);
+        config.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, Config.SecurityProtocol);
+        config.put(SaslConfigs.SASL_MECHANISM, Config.SaslMechanism);
+        config.put(SaslConfigs.SASL_JAAS_CONFIG, Config.SaslJaasConfig);
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        System.out.println("Hello World!");
+        // config.put(StreamsConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
+        //Option #1: With High Level DSL
         StreamsBuilder builder = new StreamsBuilder();
-
-        // split by space
-        KStream<String, String> stream = builder.stream(reqTopicName);
+        KStream<String, String> stream = builder.stream(Config.TopicIn);
         KTable<String, Long> wordCounts = stream.mapValues(value -> value.toLowerCase()) // lower case
-                .flatMapValues(splitTextCode -> Arrays.asList(splitTextCode.split(" "))) // split by space
-                .selectKey((ignoredKey, word) -> word) // so that we can group
-                .groupByKey().count("counts");
+                                                .flatMapValues(splitTextCode -> Arrays.asList(splitTextCode.split(" "))) // split by space
+                                                .selectKey((ignoredKey, word) -> word) // so that we can group
+                                                .groupByKey().count("counts");
 
         wordCounts.to(Serdes.String(), Serdes.Long(), resTopicName);
 
@@ -48,5 +52,12 @@ public class App {
         System.out.println(streamPipeline.toString());
 
         Runtime.getRuntime().addShutdownHook(new Thread(streamPipeline::close)); //syntatic sugar, a timeout can also do it
+
     }
 }
+
+
+        //This recovers state (offset, where it left)
+        //kafka maintains repartition and changelog topics
+        //to add more partitions, repartition is required        
+        //if onw instance is down, rabalance is automatic
